97e2b77b3bc52c094330c1a1d310ce99e21154bc
biamateii <biamateii@yahoo.com>
2020-04-21 11:30:54 +0300

    Created users based on Factory Design Pattern

---
+++ TravelAgency/TravelAgency/Factory/Types/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using TravelAgency.Factory.Enums;
+
+namespace TravelAgency.Factory.Types
+{
+    class Customer : User
+    {
+        public List<Order> Orders { get; set; }
+
+        public Customer(string username, string password):
+            base(username, password)
+        {
+            Orders = new List<Order>();
+        }
+
+        public override ERoleType RoleType()
+        {
+            return ERoleType.ECustomer;
+        }
+
+        public void Add(Order order)
+        {
+            Orders.Add(order);
+        }
+    }
+}
---
+++ TravelAgency/TravelAgency/Factory/UserFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using TravelAgency.Factory.Enums;
+
+namespace TravelAgency.Factory
+{
+    abstract class UserFactory
+    {
+
+        public abstract User GetUser(string username, string password);
+    }
+}
